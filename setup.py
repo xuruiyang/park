52c749f97867c15b343722f36db831bb2708e9df
Ruiyang Xu <xuruiyang@users.noreply.github.com>
2020-10-21 22:01:47 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='park',
+setup(name='parkenvforruiyang',
       version='0.0.1',
       install_requires=['numpy', 'wget', 'networkx', 'gym', 'dill', 'pycapnp', 'pandas'],
       url = 'https://github.com/xuruiyang/park',   # Provide either the link to your github or to your website
