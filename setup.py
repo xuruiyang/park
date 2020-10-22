fd9b21b39a23564971ed3c326230f817e1962967
Ruiyang Xu <xuruiyang@users.noreply.github.com>
2020-10-21 21:56:54 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,7 @@ from setuptools import setup
 
 setup(name='park',
       version='0.0.1',
-      install_requires=['numpy', 'wget', 'networkx', 'gym', 'dill', 'pycapnp', 'pandas']
+      install_requires=['numpy', 'wget', 'networkx', 'gym', 'dill', 'pycapnp', 'pandas'],
+      url = 'https://github.com/xuruiyang/park',   # Provide either the link to your github or to your website
+      download_url = 'https://github.com/xuruiyang/park/tree/master/dist/park-0.0.1.tar.gz',    # I explain this later on
 )
